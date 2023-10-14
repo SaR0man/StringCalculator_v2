fee6e89c5fdfca6f1b1f504440c41abc9daa9569
Roman Savin <135424483+SaR0man@users.noreply.github.com>
2023-10-14 11:48:26 +0300

    work update

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 
-class Math {
-    public static int Sum(int arg1, int arg2) {
-        return arg1 + arg2;
-    }
-}
-
 public class Main {
 
     public static String string;  // вводимая строка
     public static String[] array;  // массив для хранения обоих аргументов выражения
 
     public static void input() {  //// ввод строки
+        System.out.println("Калькулятор для простых арифметических действий с двумя аргументами");
+        System.out.println("===================================================================");
         System.out.print("Введите выражение:_");
         Scanner scanner = new Scanner(System.in);
         boolean breaker = true;  // выключатель для while
@@ -23,30 +19,53 @@ public class Main {
         }
     }
 
-    public static double output() {  //// решение выражения и вывод результата
-        double result = 0;
+    public static void sum(String arg1, String arg2) {  //// производим сложение
+        double result = Double.parseDouble(arg1) + Double.parseDouble(arg2);
+        if (result % 1 == 0)
+            System.out.println("Результат:" + (int) result);
+        else System.out.println("Результат:" + result);
+    }
+    public static void difference(String arg1, String arg2) {  //// производим вычитание
+        double result = Double.parseDouble(arg1) - Double.parseDouble(arg2);
+        if (result % 1 == 0)
+            System.out.println("Результат:" + (int) result);
+        else System.out.println("Результат:" + result);
+    }
+    public static void multiplication(String arg1, String arg2) {  //// производим умножение
+        double result = Double.parseDouble(arg1) * Double.parseDouble(arg2);
+        if (result % 1 == 0)
+            System.out.println("Результат:" + (int) result);
+        else System.out.println("Результат:" + result);
+    }
+    public static void dividing(String arg1, String arg2) {  //// производим деление
+        double result = Double.parseDouble(arg1) / Double.parseDouble(arg2);
+        if (result % 1 == 0)
+            System.out.println("Результат:" + (int) result);
+        else System.out.println("Результат:" + result);
+    }
+
+    public static void solution() {  //// решение выражения и вывод результата
         if (string.contains("+")) {
             array = string.split("[+]");  // TODO знак "+" без скобок не принимается в качестве разделителя (?)
-            result = Double.parseDouble(array[0]) + Double.parseDouble(array[1]);
+            sum(array[0], array[1]);
         }
         else if (string.contains("-")) {
             array = string.split("-");
-            result = Double.parseDouble(array[0]) - Double.parseDouble(array[1]);
+            difference(array[0], array[1]);
         }
         else if (string.contains("*")) {
             array = string.split("[*]");  // TODO знак "*" без скобок не принимается в качестве разделителя (?)
-            result = Double.parseDouble(array[0]) * Double.parseDouble(array[1]);
+            multiplication(array[0], array[1]);
         }
         else if (string.contains("/")) {
             array = string.split("/");
-            result = Double.parseDouble(array[0]) / Double.parseDouble(array[1]);
+            dividing(array[0], array[1]);
         }
         else System.out.println("Неверный ввод");
-        return result;
     }
 
     public static void main(String[] args) {
         input();
-        System.out.println("Результат: " + output());
+        solution();
     }
 }
